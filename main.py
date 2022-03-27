c457b54f05ca8fcb7e12ed73e89241d8d5a10e86
sushma <sribhanu410438@gmail.com>
2022-03-27 01:57:31 -0500

    pushing change initially

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, render_template
-application = Flask(__name__)
+app = Flask(__name__)
 
-application.route("/")
+@app.route("/")
 def hello():
     return render_template ("index.html")
 
-application.route("/newproject")
+@app.route("/newproject")
 def newproject():
     return render_template ("newproject.html")
 
-application.route("/projects")
+@app.route("/projects")
 def projects():
     return render_template ("projects.html")
 
-application.route("/education")
+@app.route("/education")
 def education():
     return render_template ("education.html")
 
-application.route("/skillset")
+@app.route("/skillset")
 def skillset():
     return render_template ("skillset.html")
 
-# application.route('/newprojects', methods=['POST'])
+# @app.route('/newproject', methods=['POST'])
 # def saveproject():
 #     input_nopol = request.form['text_box']
 #     if request.method == 'POST':
 #        with open('nopol.txt', 'w') as f:
 #             f.write(str(input_nopol))
 #     return render_template('index.html', nopol=input_nopol)
+
+if __name__ == "__main__":
+    app.run()
